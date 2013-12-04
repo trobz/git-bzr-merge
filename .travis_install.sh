6044253131e1927c14558a79a4f1c0d849815400
Michel Meyer <michel@zazabe.fr>
2013-12-04 17:34:32 +0000

    fix travis, bzr version update

--- .travis_install.sh
+++ .travis_install.sh
@@ -5,15 +5,14 @@ sudo apt-get -ym install wget
 
 # install dependencies
 sudo apt-get -ym install git
+# remove bazaar to reinstall the latest version
+sudo apt-get -y remove bzr python-bzrlib 
+sudo apt-get -y autoremov
 # install bazaar from latest package
 wget http://ftp.us.debian.org/debian/pool/main/b/bzr/python-bzrlib_2.6.0~bzr6526-1_amd64.deb
 wget http://ftp.us.debian.org/debian/pool/main/b/bzr/bzr_2.6.0~bzr6526-1_all.deb
-wget http://ftp.us.debian.org/debian/pool/main/b/bzr/python-bzrlib_2.6.0-3_amd64.deb
 sudo dpkg -i python-bzrlib_2.6.0~bzr6526-1_amd64.deb
 sudo dpkg -i bzr_2.6.0~bzr6526-1_all.deb
-sudo apt-get -f -y install
-# install python lib
-sudo dpkg -i python-bzrlib_2.6.0-3_amd64.deb
 sudo apt-get -f -y install
 
 # install git-remote-bzr
