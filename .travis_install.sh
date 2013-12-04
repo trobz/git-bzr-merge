b8fd386f1d34ebd5f38015a2bd508e6272450ac8
Michel Meyer <michel@zazabe.fr>
2013-12-04 15:46:40 +0000

    update travis install script

--- .travis_install.sh
+++ .travis_install.sh
@@ -10,9 +10,10 @@ wget http://ftp.us.debian.org/debian/pool/main/b/bzr/python-bzrlib_2.6.0~bzr6526
 wget http://ftp.us.debian.org/debian/pool/main/b/bzr/bzr_2.6.0~bzr6526-1_all.deb
 sudo dpkg -i python-bzrlib_2.6.0~bzr6526-1_amd64.deb
 sudo dpkg -i bzr_2.6.0~bzr6526-1_all.deb
-sudo apt-get -f -y install 
+sudo apt-get -f -y install
 
-wget https://raw.github.com/felipec/git/fc/master/git-remote-bzr.py -O /usr/local/bin/git-remote-bzr
+# install git-remote-bzr
+sudo wget https://raw.github.com/felipec/git/fc/master/git-remote-bzr.py -O /usr/local/bin/git-remote-bzr
 
 # install git-bzr-merge
 sudo cp git-bzr-merge /usr/local/bin/
