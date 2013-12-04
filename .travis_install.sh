b8c7cb15026fd08fadb849a3593c13268e6769d7
Michel Meyer <michel@zazabe.fr>
2013-12-04 15:39:20 +0000

    update travis install script, install latest bzr package

--- .travis_install.sh
+++ .travis_install.sh
@@ -4,7 +4,14 @@
 sudo apt-get -ym install wget
 
 # install dependencies
-sudo apt-get -ym install git bzr
+sudo apt-get -ym install git
+# install bazaar from latest package
+wget http://ftp.us.debian.org/debian/pool/main/b/bzr/python-bzrlib_2.6.0~bzr6526-1_amd64.deb
+wget http://ftp.us.debian.org/debian/pool/main/b/bzr/bzr_2.6.0~bzr6526-1_all.deb
+sudo dpkg -i python-bzrlib_2.6.0~bzr6526-1_amd64.deb
+sudo dpkg -i bzr_2.6.0~bzr6526-1_all.deb
+sudo apt-get -f -y install 
+
 wget https://raw.github.com/felipec/git/fc/master/git-remote-bzr.py -O /usr/local/bin/git-remote-bzr
 
 # install git-bzr-merge
